fc4ea32535e8d1992e9e833e14630b35d5d44027
Kamari <kmoore971@gmail.com>
2023-10-08 18:48:12 -0500

    Update cpu move funct

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function renderGameBoard() {
 };
 
 // Player factory function
-function Player(name, symbol) {
+function Player(name, symbol, isCPU = false) {
     const makeMove = (row, col) => {
         return gameBoard.setCell(row, col, symbol);
     };
@@ -103,6 +103,7 @@ function Player(name, symbol) {
     return {
         name,
         symbol,
+        isCPU,
         makeMove
     };
 };
@@ -113,8 +114,8 @@ let draws = 0;
 let oWins = 0;
 
 // Create players
-const playerX = Player('Player X', 'X');
-const playerO = Player('Player O', 'O');
+let playerX = Player('Player X', 'X');
+let playerO = Player('Player O', 'O');
 
 // Test factory function attributes
 console.log(playerX.symbol);
@@ -243,8 +244,124 @@ renderGameBoard();
 // renderGameBoard();
 // console.log(gameBoard.getBoard());
 
+// Display the initial setup modal when the page loads
+document.getElementById('setup-modal').style.display = 'block';
 
-// Task
-// create a reset button that sets the scores and ties back to 0 when preseed
-// Add player name input (based on how many players) - changes player to custom name
-// Add cpu input - randomize cpu picks on board based on empty spaces and users value
+// Function to handle the initial setup using alerts
+function handlePlayerSetup() {
+    const numPlayersInput = document.getElementById('num-players').value;
+    const player1SetupModal = document.getElementById('player1-setup-modal');
+    const player2SetupModal = document.getElementById('player2-setup-modal');
+    const player2SymbolDisplay = document.getElementById('player2-symbol-display');
+
+    if (numPlayersInput == 1) {
+        // If 1 player is selected, show the Player setup modal for Player 1
+        player1SetupModal.style.display = 'block';
+        player2SetupModal.style.display = 'none'; // Hide Player 2's modal
+
+        // Hide the initial setup modal
+        document.getElementById('setup-modal').style.display = 'none';
+    } else if (numPlayersInput == 2) {
+        // If 2 players are selected, show the Player 1's setup modal
+        player1SetupModal.style.display = 'none';
+
+        // Show Player 2's modal
+        player2SetupModal.style.display = 'block';
+        const player1Symbol = document.getElementById('player1-symbol').value;
+        player2SymbolDisplay.textContent = (player1Symbol === 'X') ? 'O' : 'X';
+
+        // Hide the initial setup modal
+        document.getElementById('setup-modal').style.display = 'none';
+
+        console.log("Next Modal")
+    }
+}
+
+// Function to update Player 2's symbol in the Player 2 setup modal
+function updatePlayer2Symbol() {
+    const player1Symbol = document.getElementById('player1-symbol-2').value;
+    document.getElementById('player2-symbol-display').textContent = (player1Symbol === 'X') ? 'O' : 'X';
+}
+
+
+// Function to start the game
+function startGame() {
+    const numPlayersInput = document.getElementById('num-players').value;
+    const player1Symbol = document.getElementById('player1-symbol').value;
+    const player1Name = document.getElementById('player1-name').value;
+    const player2Symbol = document.getElementById('player2-symbol-display').textContent;
+    const player2Name = document.getElementById('player2-name').value;
+
+    // Determine which player chose 'X' and assign accordingly
+    playerX = (player1Symbol === 'X') ? Player(player1Name, 'X') : Player(player2Name, 'X');
+
+    // Determine which player chose 'O' and assign accordingly
+    playerO = (player1Symbol === 'O') ? Player(player1Name, 'O') : Player(player2Name, 'O');
+
+
+    // Set current player to player1
+    currentPlayer = playerX;
+
+    if (numPlayersInput === 1 && player1Symbol === 'X') {
+        playerO.isCPU = true;
+    } else if (numPlayersInput === 1 && player1Symbol === 'O') {
+        playerX.isCPU = true;
+    }
+
+    // Initialize the game board (clear previous state)
+    gameBoard.clearBoard();
+
+    // Render the initial state of the game board
+    renderGameBoard();
+
+    // Hide the setup modals
+    document.getElementById('setup-modal').style.display = 'none';
+    document.getElementById('player1-setup-modal').style.display = 'none';
+    document.getElementById('player2-setup-modal').style.display = 'none';
+
+    // If there's only one player playing and it's a CPU, make a move after a short delay
+    if (numPlayersInput == 1 && currentPlayer.isCPU) {
+        // currentPlayer.name = 'CPU'
+        currentPlayer = playerO;
+        setTimeout(makeCPUmove, 500);
+    }
+
+    // console.log(currentPlayer.symbol)
+}
+
+
+// Function to make a move for the CPU player
+function makeCPUmove() {
+    // Generate random row and column indices for the CPU move
+    let row, col;
+    do {
+        row = Math.floor(Math.random() * 3);
+        col = Math.floor(Math.random() * 3);
+        // keep generating random coordinates until find empty cell
+    } while (!gameBoard.setCell(row, col, currentPlayer.symbol));
+
+    // Update the game board display
+    renderGameBoard();
+
+    // Check if the CPU has won or if it's a draw
+    if (checkWinner()) {
+        // Delay the alert to allow time for rendering
+        setTimeout(() => {
+            // Update win counters
+            updateWins();
+            alert(`${currentPlayer.name} Won!`);
+            resetGame();
+        }, 0);
+    } else if (checkDraw()) {
+        // Delay the alert to allow time for rendering
+        setTimeout(() => {
+            // Update tie counter
+            updateDraws();
+            alert(`It's a Draw!`);
+            resetGame();
+        }, 0);
+    } else {
+        // Switch to the other player
+        switchPlayer();
+    }
+}
